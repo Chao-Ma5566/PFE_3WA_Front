9ecca2801021cd5f69b6005405c780ace2e5715d
chao.ma <mjc1378098@gmail.com>
2024-06-17 09:36:05 +0200

    feat: redeploiyer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
+import App from './App.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App.js';
 import StoreProvider from './components/StoreProvider.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
